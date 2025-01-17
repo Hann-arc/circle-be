a369b58682313e34ae739c880481add4d4ab4f63
Muhammad Farhaan <schamlos17@gmail.com>
2025-01-18 00:19:25 +0700

    update entry point

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
-import router from "./routes/index";
+import router from "./src/routes";
 import cors from 'cors'
 
 const app = express();
